69709b29deac13b1a610f84b4ded0ff880a6801d
Aarti <aartipathak129@gmail.com>
2021-07-12 16:25:42 -0400

    Added Application Status pages. Functionality to check the application status by email.

---
+++ application_status.py
@@ -0,0 +1,40 @@
+from flask import request, render_template, redirect, flash
+import dbconnection
+
+
+def get_check_application_status_page():
+    return render_template("application_status_check.html")
+
+
+def check_application_status():
+    email = request.form["email"]
+    query = {'email': email.lower()}
+    collection_name = dbconnection.db["application_status"]
+
+    status_records = collection_name.find(query)
+    if not status_records:
+        flash("No record found.", 'error')
+        return redirect('/application/status')
+    else:
+        result = []
+        for record in status_records:
+            application_details = get_application_details(record['application_id'])
+            record['application_details'] = application_details
+
+            course_details = get_course_details(application_details['course_id'])
+            record['course_details'] = course_details
+            result.append(record)
+
+        return render_template("application_status.html", status_records=record, email=email.lower())
+
+
+def get_application_details(application_id):
+    query = {'_id': application_id}
+    collection_name = dbconnection.db["admission_applications"]
+    return collection_name.find_one(query)
+
+
+def get_course_details(course_id):
+    query = {'_id': course_id}
+    collection_name = dbconnection.db["courses"]
+    return collection_name.find_one(query)
