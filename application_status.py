c32cfcd17c21e41e2a68046c8896ec3bf617596b
Aarti <aartipathak129@gmail.com>
2021-08-03 00:13:57 -0400

    Added check while applying for a course if it is already applied by the user.

--- application_status.py
+++ application_status.py
@@ -3,6 +3,7 @@ from flask import request, render_template, redirect, flash
 import dbconnection
 
 
+
 def get_check_application_status_page():
     return render_template("application_status_check.html", title="Check Application Status")
 
@@ -18,15 +19,31 @@ def check_application_status():
         return redirect('/application/status')
     else:
         result_list = []
+        decided_course_id = []
         for record in status_records:
             application_details = get_application_details(record['application_id'])
             record['application_details'] = application_details
 
-            course_details = get_course_details(ObjectId(str(application_details['course_id'])))
+            course_id = ObjectId(str(application_details['course_id']))
+            course_details = get_course_details(course_id)
             record['course_details'] = course_details
             result_list.append(record)
+            decided_course_id.append(course_id)
 
-        return render_template("application_status.html", status_records=result_list, email=email.lower(), title="Application Status")
+        query = {'email': email.lower(), 'course_id': { '$nin': decided_course_id }}
+        collection_name = dbconnection.db["admission_applications"]
+        pending_application_records = collection_name.find(query)
+
+        for application_details in pending_application_records:
+            pending_record = {'application_details': application_details}
+            course_id = ObjectId(str(application_details['course_id']))
+            course_details = get_course_details(course_id)
+            pending_record['course_details'] = course_details
+            pending_record['status'] = 'PEND'
+            result_list.append(pending_record)
+
+        return render_template("application_status.html", status_records=result_list,
+                               email=email.lower(), title="Application Status")
 
 
 def get_application_details(application_id):
