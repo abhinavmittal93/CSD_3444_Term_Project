11ed1ce353ee10d52e4c58faac4f588f8547e74c
Aarti <aartipathak129@gmail.com>
2021-08-05 03:22:46 -0400

    Added a page to view all the pending admission applications and now admin can view all the details of a particular application.

--- admission_applications.py
+++ admission_applications.py
@@ -1,6 +1,56 @@
+from bson import ObjectId
+from flask import render_template, flash, redirect
 import dbconnection
+import admin_courses
+
 
 def get_admission_application_by_course_and_email(course_id, email):
     query = {'course_id': course_id, 'email': email}
     collection_name = dbconnection.db["admission_applications"]
     return collection_name.find_one(query)
+
+
+def get_pending_admission_applications_page():
+    try:
+        pending_applications_list = get_pending_admission_applications()
+        return render_template("admin_admission_applications.html", pending_applications_list=pending_applications_list,
+                               title='Admission Applications')
+    except Exception as e:
+        print(e)
+        flash("Error occurred. Please try again!", 'error')
+        return redirect('/admin/courses')
+
+
+def get_pending_admission_applications():
+    application_status_coll = dbconnection.db["application_status"]
+    decided_applications_id = application_status_coll.find()
+    decided_applications_id_list = []
+
+    for decided_applications in decided_applications_id:
+        decided_applications_id_list.append(decided_applications['application_id'])
+
+    admission_applications_collection = dbconnection.db["admission_applications"]
+    pending_applications_query = {'_id': {'$nin': decided_applications_id_list}}
+    pending_applications = admission_applications_collection.find(pending_applications_query)
+
+    pending_applications_list = []
+
+    for application in pending_applications:
+        course_details = admin_courses.get_course_details_by_id(application['course_id'])
+        application['course_details'] = course_details
+        pending_applications_list.append(application)
+
+    return pending_applications_list
+
+
+def get_application_by_id(application_id):
+    try:
+        query = {'_id': ObjectId(str(application_id))}
+        collection_name = dbconnection.db["admission_applications"]
+        application_details = collection_name.find_one(query)
+        return render_template("admin_admission_applications_view.html", application_details=application_details,
+                               title='Admission Application')
+    except Exception as e:
+        print(e)
+        flash("Error occurred. Please try again!", 'error')
+        return redirect('/admin/admission/applications')
--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from flask_session import Session
 import application_status
 from user import User
 import Contact_Us
+import admission_applications
 
 app = Flask(__name__)
 toastr = Toastr(app)
-
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -24,20 +24,21 @@ app.add_url_rule('/login', view_func=login.login_page)
 app.add_url_rule('/authenticate', view_func=login.authenticate_admin, methods=['POST'])
 app.add_url_rule('/logout', view_func=login.logout)
 
-
 app.add_url_rule('/admin/courses', view_func=admin_courses.get_admin_course_list_page)
 app.add_url_rule('/admin/courses/delete/<string:object_id>', endpoint='delete_course', view_func=admin_courses.delete_course)
 app.add_url_rule('/admin/course/new', view_func=admin_courses.get_add_new_course_page)
 app.add_url_rule('/admin/course/edit/<string:course_id>', endpoint='get_edit_course_page', view_func=admin_courses.get_edit_course_page)
 app.add_url_rule('/admin/course/save', view_func=admin_courses.save_course, methods=['POST'])
+app.add_url_rule('/admin/admission/applications', view_func=admission_applications.get_pending_admission_applications_page)
+app.add_url_rule('/admin/admission/applications/<string:application_id>', endpoint='get_application_by_id', view_func=admission_applications.get_application_by_id)
 
 app.add_url_rule('/contactus', view_func=Contact_Us.get_contact_us_page)
 app.add_url_rule('/contactus/save', view_func=Contact_Us.save_contact_us_details, methods=['POST'])
 
 app.add_url_rule('/courses', view_func=courses.get_courses_page)
-app.add_url_rule('/course/apply/<string:course_id>', endpoint='get_course_application_page',  view_func=courses.get_course_application_page)
+app.add_url_rule('/course/apply/<string:course_id>', endpoint='get_course_application_page', view_func=courses.get_course_application_page)
 app.add_url_rule('/course/apply/save', view_func=courses.apply_course, methods=['POST'])
-app.add_url_rule('/course/<string:course_id>', endpoint='get_course_view_page',  view_func=courses.get_course_view_page)
+app.add_url_rule('/course/<string:course_id>', endpoint='get_course_view_page', view_func=courses.get_course_view_page)
 
 app.add_url_rule('/application/status', view_func=application_status.get_check_application_status_page)
 app.add_url_rule('/application/status/check', view_func=application_status.check_application_status, methods=['POST'])
@@ -60,6 +61,7 @@ def admin():
         if not session.get("email"):
             return redirect('/login')
 
+
 @app.route("/add_new_user")
 def add_new_user():
     user = User("Abhinav Mittal", "abhinavmittal93@gmail.com", "@Bhinav123")
@@ -78,6 +80,7 @@ def handle_page_not_found_error(e):
 @app.errorhandler(500)
 def handle_internal_server_error(e):
     return render_template('500.html', title="Internal Server Error"), 500
+
 
 if __name__ == '__main__':
     toastr.init_app(app)
